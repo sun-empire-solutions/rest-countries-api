c1a713d00437ec61018a594022cd85a4440ea8a5
cteyr123 <cteyr123@gmail.com>
2022-04-24 12:35:33 -0400

    container-list-country

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,7 @@
-import { useEffect, useState } from "react";
-
-import { api } from "./api";
-import { Country } from "./types";
+import { Container } from "./container/Container";
 
 const App = () => {
-  const [countries, setCountries] = useState<Country[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const getCountries = async () => {
-    try {
-      const result = await api.get("all");
-      setCountries(result);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getCountries();
-  }, []);
-
-  return isLoading ? (
-    <h1>Loading...</h1>
-  ) : (
-    <ul>
-      {countries.map((item, index) => (
-        <li key={index}>
-          {item.name.common}
-          <img src={item.flags.png} alt="" />
-        </li>
-      ))}
-    </ul>
-  );
+  return <Container />;
 };
 
 export { App };
