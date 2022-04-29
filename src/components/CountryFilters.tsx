eff1423ce6765a06c46f1d56945ae8a0e871ca34
cteyr123 <cteyr123@gmail.com>
2022-04-29 02:37:14 -0400

    add-icon-remove-and-funcionality

--- src/components/CountryFilters.tsx
+++ src/components/CountryFilters.tsx
@@ -1,6 +1,10 @@
-import { ChangeEvent } from "react";
+import React, { ChangeEvent } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 //@ts-ignore
 import search from "./../assets/images/search.svg";
+//@ts-ignore
+import remove from "./../assets/images/remove.svg";
 
 const CountryFilters = ({
   nameFilter,
@@ -8,12 +12,24 @@ const CountryFilters = ({
   regionFilter,
   setRegionFilter,
 }: IProps) => {
+  const [State, setState] = useState("invisible");
+
   const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
     setNameFilter(event.target.value);
+    if (event.target.value === "") {
+      setState("invisible");
+    } else {
+      setState("visible");
+    }
   };
 
   const handleRegionSelect = (event: ChangeEvent<HTMLSelectElement>) => {
     setRegionFilter(event.target.value);
+  };
+
+  const clearInput = () => {
+    setNameFilter("");
+    setState("invisible");
   };
 
   return (
@@ -26,8 +42,11 @@ const CountryFilters = ({
           onChange={handleNameChange}
           placeholder="Search for a country..."
         />
-        <div className="img-icon">
+        <div className="img-icon-search">
           <img src={search} alt="" />
+        </div>
+        <div className={`img-icon-remove ${State}`} onClick={clearInput}>
+          <img src={remove} alt="" />
         </div>
       </div>
 
@@ -36,9 +55,7 @@ const CountryFilters = ({
         onChange={handleRegionSelect}
         value={regionFilter}
       >
-        <option selected value="All">
-          All
-        </option>
+        <option value="All">All</option>
         <option value="Africa">Africa</option>
         <option value="America">America</option>
         <option value="Asia">Asia</option>
